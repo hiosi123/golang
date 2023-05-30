8e0948861a1a3c1283f25c7da4d45178f9f8cb7d
hiosi123 <tls1641@gmail.com>
2023-05-30 23:55:21 +0900

    function

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	design "dojo/golangDesign"
+	"dojo/function"
+	"fmt"
 )
 
 func main() {
-	design.DesignPattern()
+	result := function.Sum(1, 2)
+	fmt.Println(result)
+
+	result1, ok := function.GeertingOk("hello")
+	if ok {
+		fmt.Println(result1)
+	}
+
+	result3 := function.Fibonacci(10)
+	fmt.Println(result3)
+
+	param := function.ReturnParam(2)
+	defer fmt.Println(param)
+
+	function := function.ReturnFunction(1, function.ReturnParam)
+	defer fmt.Println(function)
+
 }
--- function/func_golang.go
+++ function/func_golang.go
@@ -1,7 +1,75 @@
 package function
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func Function() {
 	fmt.Println("hello")
+
+}
+
+func Variadic(intergers ...int) {
+	for _, integer := range intergers {
+		fmt.Println(integer)
+	}
+}
+
+func Sum(a, b int) int {
+	return a + b
+}
+
+func NameLength(name string) (string, int) {
+	return name, len(name)
+}
+
+func Greeting(name string) (string, error) {
+	if len(name) == 0 {
+		return "", errors.New("empty name")
+	} else {
+		return "Hello " + name, nil
+	}
+}
+
+func GeertingOk(name string) (string, bool) {
+	if len(name) == 0 {
+		return "", false
+	} else {
+		return "Hello " + name, true
+	}
+}
+
+func Fibonacci(i int) int {
+	if i == 0 {
+		return 0
+	} else if i == 1 {
+		return 1
+	} else {
+		return Fibonacci(i-1) + Fibonacci(i-2)
+	}
+}
+
+func WithPointer(i *int) {
+	*i = *i + 1
+}
+
+func ReturnParam(i int) int {
+	return i
+}
+
+func ReturnFunction(i int, f func(int) int) int {
+	return f(i)
+}
+
+func First() {
+	fmt.Println("first")
+}
+
+func Second() {
+	fmt.Println("Second")
+}
+
+func CrazyFunction(f func(func()) func()) func(func()) func() {
+	return f
 }
